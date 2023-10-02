53e475ce90252629e52b31ac6663cdfbb6bb4d7a
Dmytro Kasperovych <dimakasperovich1720@gmail.com>
2023-10-02 14:00:36 -0700

    homework added

---
+++ sets/homework.py
@@ -0,0 +1,43 @@
+european_airlinis = {
+    'Eurowings',
+    'FLN Frisia ',
+    'Lübeck Air',
+    'Lufthansa',
+    'Air New Zealand',
+    'OFD ',
+    'TUI fly ',
+    'AeroLogic',
+    'Swiss',
+    'Air France',
+    'European Air',
+}
+
+international_airlinis = {
+    'Swiss',
+    'Air France',
+    'British Airways',
+    'KLM Royal Dutch Airlines',
+    'Air New Zealand',
+    'Virgin Atlantic',
+    'Qantas',
+    'Japan Airlines',
+    'Cathay Pacific',
+    'All Nippon Airways',
+    'Etihad Airways',
+    'Emirates',
+    'Qatar Airways',
+    'Turkish Airlines',
+    'Singapore Airlines',
+    'Lufthansa',
+}
+
+print(european_airlinis & international_airlinis)
+# Descrription:
+
+print(european_airlinis | international_airlinis)
+# Descrription:
+
+print(european_airlinis - international_airlinis)
+# Descrription:
+
+print(european_airlinis ^ international_airlinis)
